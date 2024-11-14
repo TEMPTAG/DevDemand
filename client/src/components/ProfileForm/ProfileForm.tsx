f4b5887c5a3da8cd61e6efa5c94a0029d9623453
Rory Dowse <rorydowse@hotmail.com>
2024-11-13 19:30:57 -0700

    add ContactForm

--- client/src/components/ProfileForm/ProfileForm.tsx
+++ client/src/components/ProfileForm/ProfileForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import validateProfileForm from './ValidateProfileForm.jsx';
+import validateProfileForm from './ValidateProfileForm.tsx';
 import './ProfileForm.css';
 
 export default function ProfileForm() {
@@ -62,107 +62,109 @@ export default function ProfileForm() {
     };
 
     return (
-        <form
-            onSubmit={handleSubmit}
-            className="profile-form"
-        >
-            <div className="form-group">
-                <label htmlFor="firstName">First Name:</label>
-                <input
-                    type="text"
-                    id="firstName"
-                    name="firstName"
-                    placeholder="Enter your first name"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className="form-control"
-                />
-            </div>
+        <div className="profile-form-container">
+            <form
+                onSubmit={handleSubmit}
+                className="profile-form"
+            >
+                <div className="form-group">
+                    <label htmlFor="firstName">First Name:</label>
+                    <input
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        placeholder="Enter your first name"
+                        value={formData.firstName}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className="form-control"
+                    />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="lastName">Last Name:</label>
-                <input
-                    type="text"
-                    id="lastName"
-                    name="lastName"
-                    placeholder="Enter your last name"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className="form-control"
-                />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="lastName">Last Name:</label>
+                    <input
+                        type="text"
+                        id="lastName"
+                        name="lastName"
+                        placeholder="Enter your last name"
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className="form-control"
+                    />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="location">Location:</label>
-                <input
-                    type="text"
-                    id="location"
-                    name="location"
-                    placeholder="Enter your location"
-                    value={formData.location}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className="form-control"
-                />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="location">Location:</label>
+                    <input
+                        type="text"
+                        id="location"
+                        name="location"
+                        placeholder="Enter your location"
+                        value={formData.location}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className="form-control"
+                    />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="hourlyRate">Hourly Rate:</label>
-                <input
-                    type="number"
-                    id="hourlyRate"
-                    name="hourlyRate"
-                    placeholder="Enter your hourly rate"
-                    value={formData.hourlyRate}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className="form-control"
-                />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="hourlyRate">Hourly Rate:</label>
+                    <input
+                        type="number"
+                        id="hourlyRate"
+                        name="hourlyRate"
+                        placeholder="Enter your hourly rate"
+                        value={formData.hourlyRate}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className="form-control"
+                    />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="skillset">Skillset:</label>
-                <input
-                    type="text"
-                    id="skillset"
-                    name="skillset"
-                    placeholder="Enter your skillset (i.e. HTML, CSS, Javascript)"
-                    value={formData.skillset.join(', ')} // Join array into a comma-separated string for display in the input
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className="form-control"
-                />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="skillset">Skillset:</label>
+                    <input
+                        type="text"
+                        id="skillset"
+                        name="skillset"
+                        placeholder="Enter your skillset (i.e. HTML, CSS, Javascript)"
+                        value={formData.skillset.join(', ')} // Join array into a comma-separated string for display in the input
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className="form-control"
+                    />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="bio">Bio:</label>
-                <textarea
-                    id="bio"
-                    name="bio"
-                    placeholder="Enter your bio"
-                    value={formData.bio}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className="form-control"
-                />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="bio">Bio:</label>
+                    <textarea
+                        id="bio"
+                        name="bio"
+                        placeholder="Enter your bio"
+                        value={formData.bio}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className="form-control"
+                    />
+                </div>
 
-            <div className="form-group">
-                {/* Update to dynamically load button */}
-                <button type="submit" className="btn btn-primary">Submit</button> 
-            </div>
+                <div className="form-group">
+                    {/* Update to dynamically load button */}
+                    <button type="submit" className="btn btn-primary">Submit</button> 
+                </div>
 
-            {/* Display form errors if any */}
-            <div className="form-errors">
-                {errors.firstName && <p className="form-error">{errors.firstName}</p>}
-                {errors.lastName && <p className="form-error">{errors.lastName}</p>}
-                {errors.location && <p className="form-error">{errors.location}</p>}
-                {errors.hourlyRate && <p className="form-error">{errors.hourlyRate}</p>}
-                {errors.skillset && <p className="form-error">{errors.skillset}</p>}
-                {errors.bio && <p className="form-error">{errors.bio}</p>}
-            </div>
-        </form>
+                {/* Display form errors if any */}
+                <div className="form-errors">
+                    {errors.firstName && <p className="form-error">{errors.firstName}</p>}
+                    {errors.lastName && <p className="form-error">{errors.lastName}</p>}
+                    {errors.location && <p className="form-error">{errors.location}</p>}
+                    {errors.hourlyRate && <p className="form-error">{errors.hourlyRate}</p>}
+                    {errors.skillset && <p className="form-error">{errors.skillset}</p>}
+                    {errors.bio && <p className="form-error">{errors.bio}</p>}
+                </div>
+            </form>
+        </div>
     );
 }
