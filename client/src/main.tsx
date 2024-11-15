75e72b231f0252807915a16c2d2a1ebeaf56da0b
Rory Dowse <rorydowse@hotmail.com>
2024-11-14 22:37:00 -0700

    render ProfileForm on DeveloperProfile

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,7 @@ import App from "./App.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
 // import Login from "./pages/Login.tsx";
-// import Developer from "./pages/DeveloperProfile.tsx";
+import Developer from "./pages/DeveloperProfile.tsx";
 
 const router = createBrowserRouter([
   {
@@ -24,10 +24,10 @@ const router = createBrowserRouter([
       //     path: "/login",
       //     element: <Login />,
       //   },
-      //   {
-      //     path: "/developer",
-      //     element: <Developer />,
-      //   },
+        {
+          path: "/developer",
+          element: <Developer />,
+        },
       {
         path: "*",
         element: <ErrorPage />,
