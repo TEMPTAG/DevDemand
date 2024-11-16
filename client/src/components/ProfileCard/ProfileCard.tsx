35a7b73dab9f7260db4503fdd4ceaf6b94fb037e
Rory Dowse <rorydowse@hotmail.com>
2024-11-16 12:51:59 -0700

    add React Bootstrap to ProfileCard

--- client/src/components/ProfileCard/ProfileCard.tsx
+++ client/src/components/ProfileCard/ProfileCard.tsx
@@ -2,6 +2,8 @@
 // The parent component will fetch the developer data using the GraphQL query and pass it as props to this component
 
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import './ProfileCard.css';
 
 interface DeveloperProps {
@@ -19,20 +21,37 @@ interface DeveloperProps {
     };
 }
 
+const formatPhoneNumber = (number: string) => {
+    const formattedNumber = number.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+    return formattedNumber;
+};
+
 const ProfileCard: React.FC<DeveloperProps> = ({ developer }) => {
     return (
-        <div className="profile-card">
-            <h3>{developer.firstName} {developer.lastName}</h3>
-            <p>Location: {developer.city}, {developer.state}</p>
-            <p>Hourly Rate: ${developer.hourlyRate}</p>
-            <p>Telephone: {developer.telephone}</p>
-            <p>Email: {developer.email}</p>
-            <p>Portfolio: {developer.portfolioLink}</p>
-            <p>GitHub: {developer.githubLink}</p>
-            <p>Bio: {developer.bio}</p>
-            {/* Add contact button here if needed and remove telephone and email */}
-        </div>
+        <Card className="profile-card">
+            {/* <Card.Img variant="top" src={developer.imageUrl || '/path/to/default-image.jpg'} /> */}
+            <Card.Body>
+                <Card.Title>{developer.firstName} {developer.lastName}</Card.Title>
+                <Card.Text>Location: {developer.city}, {developer.state}</Card.Text>
+                <Card.Text>Hourly Rate: ${developer.hourlyRate}</Card.Text>
+                <Card.Text>Telephone: {formatPhoneNumber(developer.telephone)}</Card.Text>
+                <Card.Text>Email: {developer.email}</Card.Text>
+                <Card.Text>
+                    Portfolio: {developer.portfolioLink ? (
+                    <Card.Link href={developer.portfolioLink} target="_blank">View Portfolio</Card.Link>
+                ) : 'N/A'}
+                </Card.Text>
+                <Card.Text>
+                    GitHub: {developer.githubLink ? (
+                        <Card.Link href={developer.githubLink} target="_blank">View GitHub</Card.Link>
+                    ) : 'N/A'}
+                </Card.Text>
+                <Card.Text>Bio: {developer.bio}</Card.Text>
+                {/* Add link to contact page here if needed and remove telephone and email */}
+                <Button variant="primary" href={`mailto:${developer.email}`}>Contact</Button>
+            </Card.Body>
+        </Card>
     );
-};
+}
 
 export default ProfileCard;
