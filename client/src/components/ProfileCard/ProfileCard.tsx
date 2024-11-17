ddea4fe52a840c0fd3b5f724f13fc51deaa7dd1a
Rory Dowse <rorydowse@hotmail.com>
2024-11-17 08:27:10 -0700

    add ProfileCard placeholders

--- client/src/components/ProfileCard/ProfileCard.tsx
+++ client/src/components/ProfileCard/ProfileCard.tsx
@@ -3,10 +3,12 @@
 
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Placeholder from 'react-bootstrap/Placeholder';
 import Button from 'react-bootstrap/Button';
 import './ProfileCard.css';
 
 interface DeveloperProps {
+    isLoading: boolean;
     developer: {
         firstName: string;
         lastName: string;
@@ -26,29 +28,61 @@ const formatPhoneNumber = (number: string) => {
     return formattedNumber;
 };
 
-const ProfileCard: React.FC<DeveloperProps> = ({ developer }) => {
+const ProfileCard: React.FC<DeveloperProps> = ({ isLoading, developer }) => {
     return (
         <Card className="profile-card">
             {/* <Card.Img variant="top" src={developer.imageUrl || '/path/to/default-image.jpg'} /> */}
             <Card.Body>
-                <Card.Title>{developer.firstName} {developer.lastName}</Card.Title>
-                <Card.Text>Location: {developer.city}, {developer.state}</Card.Text>
-                <Card.Text>Hourly Rate: ${developer.hourlyRate}</Card.Text>
-                <Card.Text>Telephone: {formatPhoneNumber(developer.telephone)}</Card.Text>
-                <Card.Text>Email: {developer.email}</Card.Text>
-                <Card.Text>
-                    Portfolio: {developer.portfolioLink ? (
-                    <Card.Link href={developer.portfolioLink} target="_blank">View Portfolio</Card.Link>
-                ) : 'N/A'}
-                </Card.Text>
-                <Card.Text>
-                    GitHub: {developer.githubLink ? (
-                        <Card.Link href={developer.githubLink} target="_blank">View GitHub</Card.Link>
-                    ) : 'N/A'}
-                </Card.Text>
-                <Card.Text>Bio: {developer.bio}</Card.Text>
-                {/* Add link to contact page here if needed and remove telephone and email */}
-                <Button variant="primary" href={`mailto:${developer.email}`}>Contact</Button>
+                {isLoading ? (
+                    <>
+                        <Placeholder as={Card.Title} animation="glow">
+                            <Placeholder xs={6} />
+                        </Placeholder>
+                        <Placeholder as={Card.Text} animation="glow">
+                            <Placeholder xs={6} />
+                        </Placeholder>
+                        <Placeholder as={Card.Text} animation="glow">
+                            <Placeholder xs={6} />
+                        </Placeholder>
+                        <Placeholder as={Card.Text} animation="glow">
+                            <Placeholder xs={6} />
+                        </Placeholder>
+                        <Placeholder as={Card.Text} animation="glow">
+                            <Placeholder xs={6} />
+                        </Placeholder>
+                        <Placeholder as={Card.Text} animation="glow">
+                            <Placeholder xs={6} />
+                        </Placeholder>
+                        <Placeholder as={Card.Text} animation="glow">
+                            <Placeholder xs={6} />
+                        </Placeholder>
+                        <Placeholder as={Card.Text} animation="glow">
+                            <Placeholder xs={6} />
+                        </Placeholder>
+                        <Placeholder.Button variant="primary" xs={6} animation="glow" />
+                    </>
+                ) : (
+                    <>
+                        <Card.Title>{developer.firstName} {developer.lastName}</Card.Title>
+                        <Card.Text>Location: {developer.city}, {developer.state}</Card.Text>
+                        <Card.Text>Hourly Rate: ${developer.hourlyRate}</Card.Text>
+                        <Card.Text>Telephone: {formatPhoneNumber(developer.telephone)}</Card.Text>
+                        <Card.Text>Email: {developer.email}</Card.Text>
+                        <Card.Text>
+                            Portfolio: {developer.portfolioLink ? (
+                            <Card.Link href={developer.portfolioLink} target="_blank">View Portfolio</Card.Link>
+                        ) : 'Portfolio not provided'}
+                        </Card.Text>
+                        <Card.Text>
+                            GitHub: {developer.githubLink ? (
+                                <Card.Link href={developer.githubLink} target="_blank">View GitHub</Card.Link>
+                            ) : 'GitHub not provided'}
+                        </Card.Text>
+                        <Card.Text>Bio: {developer.bio}</Card.Text>
+                        {/* Add link to contact page here if needed and remove telephone and email */}
+                        <Button variant="primary" href={`mailto:${developer.email}`}>Contact</Button>
+                    </>
+                )}
             </Card.Body>
         </Card>
     );
