3116960c42617b1b94b7362f6ba875d2e9939d96
Rory Dowse <rorydowse@hotmail.com>
2024-11-17 19:04:14 -0700

    add Query and Mutation

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,1 +1,10 @@
 import { gql } from '@apollo/client';
+
+export const UPDATE_PROFILE = gql`
+    mutation UpdateProfile($profileInput: ProfileInput!) {
+        updateProfile(profileInput: $profileInput) {
+            success
+            message
+        }
+    }
+`;
